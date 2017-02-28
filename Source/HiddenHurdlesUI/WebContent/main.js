1d2e6165db6878ab95941c6c1c2dc920a68b16f2
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2017-02-28 05:38:48 +0530

    Commit Source

---
+++ Source/HiddenHurdlesUI/WebContent/main.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+$(document).ready(function(){
+    $("#level1").click(function(){
+    	window.location.href = "Level1.html";
+    });
+});
+
+$(document).ready(function(){
+    $("#level2").click(function(){
+    	window.location.href = "Level2.html";
+    });
+});
