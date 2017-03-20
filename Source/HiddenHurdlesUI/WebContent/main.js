18be17d17e8d6f63fee4734d696365d319933be0
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2017-03-20 08:20:20 +0530

    commit code

--- Source/HiddenHurdlesUI/WebContent/main.js
+++ Source/HiddenHurdlesUI/WebContent/main.js
@@ -13,3 +13,32 @@ $(document).ready(function(){
     	window.location.href = "Level2.html";
     });
 });
+
+function findImageType(abc){
+	 $('#txt_area').hide();
+    var tmp_img = document.createElement("img");
+    tmp_img.src = abc.src;
+	var base64 = getBase64Image(tmp_img);
+    $.ajax({
+      type: "POST",
+      url: "http://localhost:8080/get_custom",
+      data: base64,
+      success: function (result) {
+        $('#txt_area').text(result);
+        $('#txt_area').show();
+      }
+    });
+}
+
+  // sorce: http://stackoverflow.com/a/22172860
+  function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  }
+
+ 
