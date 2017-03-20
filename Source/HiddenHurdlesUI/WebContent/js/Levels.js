4fee7653ee266ade0fec429262fffeed83119911
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2017-03-21 04:37:33 +0530

    commit timer.js

---
+++ Source/HiddenHurdlesUI/WebContent/js/Levels.js
@@ -0,0 +1,12 @@
+/**
+ * Created By Manasa
+ */
+
+$(document).ready(function(){
+	$("#level1").click(function(){
+		window.location.href = "Level1.html";
+	});
+	$("#level2").click(function(){
+		window.location.href = "Level2.html";
+	});
+});
---
+++ Source/HiddenHurdlesUI/WebContent/js/timer.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Manasa
+ */
+
+var time = 3600;
+var initialOffset = '210';
+var i = 1
+
+/* Need initial run as interval hasn't yet occured... */
+$('.circle_animation').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
+
+var interval = setInterval(function() {
+		$('h2').text(i);
+		if (i == time) {  	
+      clearInterval(interval);
+			return;
+    }
+    $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
+    i++;  
+}, 1000);
