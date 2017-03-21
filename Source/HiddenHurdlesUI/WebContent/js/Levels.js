c3e62b9d5e25c15bee5ab2d765f08605cd7b714d
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2017-03-21 08:53:01 +0530

    commit levelpage

--- Source/HiddenHurdlesUI/WebContent/js/Levels.js
+++ Source/HiddenHurdlesUI/WebContent/js/Levels.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 	$("#level2").click(function(){
 		window.location.href = "Level2.html";
 	});
-$("#level2").click(function(){
+$("#level3").click(function(){
 		window.location.href = "Level3.html";
 	});
 });
