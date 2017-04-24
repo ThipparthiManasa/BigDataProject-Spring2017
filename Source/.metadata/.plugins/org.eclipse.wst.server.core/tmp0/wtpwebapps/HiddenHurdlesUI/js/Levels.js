35fc07d8b6cefe1e39a0dd78ef3610df8460a37c
ManasaReddyThipparthi <manasareddy.thipparthi@gmail.com>
2017-04-24 12:32:45 -0500

    commit

---
+++ Source/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/HiddenHurdlesUI/js/Levels.js
@@ -0,0 +1,15 @@
+/**
+ * Created By Manasa
+ */
+
+$(document).ready(function(){
+	$("#level1").click(function(){
+		window.location.href = "Level1.html";
+	});
+	$("#level2").click(function(){
+		window.location.href = "Level2.html";
+	});
+$("#level3").click(function(){
+		window.location.href = "Level3.html";
+	});
+});
